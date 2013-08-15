ee115ddbd0297d39d4c8eee63bc9903fae7660de
andylinteau <andy.linteau@buystand.com>
2013-08-15 16:46:01 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Andysblog::Application.config.secret_token = 'e81664ab4eb029a6949ed67e1eefba194119cbe69a46e1870c861eaa0cb1b04584626429a838aa1cdf5a6364f996d77829f120c55630f38a1e527a18e3f1aa49'
